91e450636b7af340bdf1e4ac0541d729f2a09cd5
hoanle-nkai <tuan204.dev@gmail.com>
2025-05-27 19:33:22 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import postRoutes from './modules/post/routes'
 import connectMongoDB from './config/mongodb'
+import cors from 'cors'
 
 const PORT = 8080
 
 connectMongoDB()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/post', postRoutes)
